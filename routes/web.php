518bfc85d77ae9d5b7d1e8abd559a006492ee71d
adrian-iqbal <iqbaladriam@gmail.com>
2025-06-19 01:48:57 +0700

    webta2

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\ExportTransaksiController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/export/transaksi/{id}', [ExportTransaksiController::class, 'export'])->name('export.transaksi');
+
+Route::get('/', function () {
+    return redirect()->route('filament.admin.auth.login');
+});
