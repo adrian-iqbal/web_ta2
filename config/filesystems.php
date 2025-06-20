97c82d62cc2833a03aad7a1b661ac06440a28ba5
adrian-iqbal <iqbaladriam@gmail.com>
2025-06-21 01:37:52 +0700

    rev3

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,17 +59,17 @@ return [
             'throw' => false,
             'report' => false,
         ],
-        'r2' => [
-  'driver' => 's3',
-  'key'    => env('AWS_ACCESS_KEY_ID'),
-  'secret' => env('AWS_SECRET_ACCESS_KEY'),
-  'region' => env('AWS_DEFAULT_REGION'),
-  'bucket' => env('AWS_BUCKET'),
-  'url'    => env('AWS_URL'),
-  'endpoint' => env('AWS_ENDPOINT'), // ditambahkan Laravel Cloud
-  'visibility' => 'public', // atau 'private'
+        'gambar-barang' => [
+    'driver' => 's3',
+    'key'    => env('AWS_ACCESS_KEY_ID'),
+    'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    'region' => env('AWS_DEFAULT_REGION'),
+    'bucket' => env('AWS_BUCKET'),
+    'url'    => env('AWS_URL'),
+    'endpoint' => env('AWS_ENDPOINT'),
+    'visibility' => 'public',
+    'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
 ],
-
     ],
 
     /*
